2402dee9c001c29910a15198619b5fdd907fd8f7
Niclas Esser <nesser@mpifr-bonn.mpg.de>
2020-11-16 15:44:56 +0100

    Working shell script

--- bash/sniffer.sh
+++ bash/sniffer.sh
@@ -10,10 +10,14 @@ o) OUTPUT=${OPTARG};;
 esac
 done
 
-
+mkdir -p tmp/
 
 for i in $(seq 0 $(($NOF_PORT-1)))
 do
     PORT=$(($START_PORT+$i))
-    echo tcpdump -i $ETH_NAME '"'port $PORT'"' -c $NUMBER_PACKETS -w '"'$PORT.pcap'"'
+    tcpdump -i $ETH_NAME '"'port $PORT'"' -c $NUMBER_PACKETS -w '"'tmp/$PORT.pcap'"'
+    python python/codif_parse.py -f tmp/$PORT.pcap -o $ETH_NAME$PORT.txt
 done
+
+rm -R tmp/
+
